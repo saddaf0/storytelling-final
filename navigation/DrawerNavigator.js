728263fb2af5c68e1784c0295e03bdecbe54c90b
saddaf0 <78302873+saddaf0@users.noreply.github.com>
2024-04-06 16:08:16 +0530

    Update DrawerNavigator.js

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -1,18 +1,48 @@
-import React from "react";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import StackNavigator from "./StackNavigator";
-import Profile from "../screens/Profile";
-import Logout from "../screens/Logout"
-const Drawer = createDrawerNavigator();
+import React from "react"
+import StackNavigator from "./stackNavigator"
+import Profile from "../screens/profile"
+import Logout from "../screens/logout"
+import {createDrawerNavigator} from "@react-navigation/drawer"
+import {NavigationContainer} from "@react-navigation/native"
+import CustomSideBarMenu from "../screens/customSideBarMenu"
+import firebase from "firebase"
 
-const DrawerNavigator = () => {
-  return (
-    <Drawer.Navigator screenOptions={{headerShown:true}}>
-      <Drawer.Screen name="Home" component={StackNavigator} />
+const Drawer = createDrawerNavigator()
+export default class DrawerNavigator extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      light_theme: true,
+    };
+  }
+  componentDidMount() {
+    let theme;
+    firebase
+      .database()
+      .ref('/users/' + firebase.auth().currentUser.uid)
+      .on('value', (snapshot) => {
+        theme = snapshot.val().current_theme;
+        this.setState({ light_theme: theme === 'light' });
+      });
+  }
+  render(){
+    var props=this.props
+    return(
+     
+      <Drawer.Navigator
+      drawerContent={(props)=> <CustomSideBarMenu {...props}/>}
+      screenOptions={{
+        drawerActiveTintColor:"#e91e63",
+        drawerInactiveTintColor:"grey",
+        itemStyle:{marginVertical:5}
+
+      }}
+      >
+      <Drawer.Screen name="Home"  component={StackNavigator} />
       <Drawer.Screen name="Profile" component={Profile} />
-       <Drawer.Screen name="Logout" component={Logout} />
-    </Drawer.Navigator>
-  );
-};
-
-export default DrawerNavigator;
+      <Drawer.Screen name="Logout" component={Logout} />
+      </Drawer.Navigator>
+      
+    )
+  }
+}
